3c6909b7c76b74053e07086a748ba765cbb0541e
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-06 12:06:00 -0300

    add functions

--- src/pages/CalculadoraContainer.js
+++ src/pages/CalculadoraContainer.js
@@ -2,23 +2,72 @@ import {useState} from 'react'
 
 import CalcDisplay from './CalcDisplay';
 function Container(){
-    let [display, setDisplay] = useState("0");
-    //let [calculo, setCalculo] = useState("")
+    let [display, setDisplay] = useState("");
+    let [calculo, setCalculo] = useState(0);
+    let [prevNumb, setPrevNumb] = useState("");
+    let [nextNumb, setNextNumb] = useState("");
+
+    let [pushOperator, setPushOperator] = useState(false)
 
     function displayNumber(e){
-        
-        console.log(e.currentTarget.childNodes[0].innerHTML)
-        
-        setDisplay(display + e.currentTarget.childNodes[0].innerHTML)
+        if(pushOperator){
+            setDisplay( e.currentTarget.childNodes[0].innerHTML)
+            setPushOperator(false)
+        } else {
+            setDisplay(display + e.currentTarget.childNodes[0].innerHTML)
+        }
+    }
+    function operators(e){
+        let operator = e.currentTarget.childNodes[0].innerHTML;
+        setPushOperator(true)
+        if(!calculo){
+            setCalculo(Number(display));
+            return;
+        }
+        console.log(operator)
+        switch(operator){
+            case "+":
+                setCalculo(calculo + Number(display));
+                break;
+            case "-":
+                setCalculo(calculo - Number(display));
+                break;
+            case "*":
+                setCalculo(calculo * Number(display));
+                break;    
+            case "/":
+                setCalculo(calculo / Number(display));
+                break;
+            default:
+                setCalculo(display)
+                break;    
+
+        }
+    }
+    function reset(e){
+        setDisplay("");
+        setCalculo(0);
+    }
+    function equal(e){
+        setDisplay(calculo)
+    }
+    let displayValue;
+    if(display){
+        displayValue= display;
+    } else {
+        displayValue = "0";
     }
 
     return (
         <section className="container">
             <div className="wrapper">
-                <div id="display">{display} </div>
-                <CalcDisplay display={display} displayNumber={displayNumber} />  
+                <div id="display">
+                    {displayValue}
+                    
+                </div>
+                <CalcDisplay display={display} displayNumber={displayNumber} operators={operators} reset={reset} equal={equal}/>  
             </div>
-
+            {calculo}
         </section>
     )
 }
