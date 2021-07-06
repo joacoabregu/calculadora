da34ac159111bccf1edf87a2a39686a6e2274337
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-06 12:06:59 -0300

    onclick functions

--- src/pages/CalcDisplay.js
+++ src/pages/CalcDisplay.js
@@ -5,32 +5,34 @@ function CalcDisplay(props) {
     return (
         <>
                 <div className="row">
-                    <Buttons symbol="1" displayNumber={props.displayNumber}/>
-                    <Buttons symbol="2" displayNumber={props.displayNumber}/>
-                    <Buttons symbol="3" displayNumber={props.displayNumber}/>
+                    <Buttons symbol="1" onClickFunction={props.displayNumber}/>
+                    <Buttons symbol="2" onClickFunction={props.displayNumber}/>
+                    <Buttons symbol="3" onClickFunction={props.displayNumber}/>
                 </div>
                 <div className="row">
-                    <Buttons  symbol="4" displayNumber={props.displayNumber}/>
-                    <Buttons symbol="5" />
-                    <Buttons symbol="6" />
+                    <Buttons  symbol="4" onClickFunction={props.displayNumber}/>
+                    <Buttons symbol="5" onClickFunction={props.displayNumber} />
+                    <Buttons symbol="6" onClickFunction={props.displayNumber} />
                 </div>
                 <div className="row">
-                    <Buttons  symbol="7" />
-                    <Buttons symbol="8" />
-                    <Buttons symbol="9" />
+                    <Buttons  symbol="7" onClickFunction={props.displayNumber} />
+                    <Buttons symbol="8" onClickFunction={props.displayNumber}/>
+                    <Buttons symbol="9" onClickFunction={props.displayNumber} />
                 </div>
                 <div className="row">
-                    <Buttons symbol="0" />
-                    <Buttons symbol="," />
-                    <Buttons symbol="+" />
+                    <Buttons symbol="0" onClickFunction={props.displayNumber} />
+                    <Buttons symbol="," onClickFunction={props.displayNumber} />
+                    <Buttons symbol="+" onClickFunction={props.operators} />
                 </div>
                 <div className="row">
-                    <Buttons  symbol="-" />
-                    <Buttons symbol="*" />
-                    <Buttons symbol="/" />
+                    <Buttons  symbol="-" onClickFunction={props.operators} />
+                    <Buttons symbol="*" onClickFunction={props.operators} />
+                    <Buttons symbol="/" onClickFunction={props.operators} />
                 </div>
                 <div className="row row-last">
-                    <Buttons  symbol="=" />
+                    <Buttons  symbol="=" onClickFunction={props.equal} />
+                    <Buttons  symbol="AC" onClickFunction={props.reset} />
+
                     
                 </div>
         </>
