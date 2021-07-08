fc5a50e3e0399f86fa649f96eafe1d6590d9caa2
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-07 22:25:40 -0300

    add reducers

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import CalculatorApp from './state/reducers';
+
+let store = createStore(CalculatorApp);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider> 
   </React.StrictMode>,
   document.getElementById('root')
 );
