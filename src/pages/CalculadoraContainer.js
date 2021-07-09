89011b16a9d47381197366648ac23e6b70288ad0
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-09 16:43:22 -0300

    removed functions

--- src/pages/CalculadoraContainer.js
+++ src/pages/CalculadoraContainer.js
@@ -1,41 +1,44 @@
-import {useState, useEffect} from 'react';
+//import {useState, useEffect} from 'react';
 import CalcDisplay from './CalcDisplay';
+import {connect} from 'react-redux';
+//import {setDisplay, setTotal,setPrevNumb, setNextNumb, setOperator, setPush} from '../state/action'
 
-function Container(){
-    let [display, setDisplay] = useState("");
-    let [total, setTotal] = useState("0");
+
+function Container(props){
+    //let [display, setDisplay] = useState("");
+    /* let [total, setTotal] = useState("0");
     let [prevNumb, setPrevNumb] = useState("");
     let [nextNumb, setNextNumb] = useState("");
-    let [operatorValue, setOperatorValue] = useState("");
-    let [pushOperator, setPushOperator] = useState(true);
+    let [operatorValue, setOperatorValue] = useState(""); */
+    //let [pushOperator, setPushOperator] = useState(true);
 
-    useEffect(() => {
+    /* useEffect(() => {
         if(isNaN(display)){       
-            reset();
+            //reset();
             alert("Enter valid number")       
         }
         if(pushOperator){          
-            setPrevNumb(display);
+            dispatch(setPrevNumb(display));
         } else {
-            setNextNumb(display);                
+            dispatch(setNextNumb(display));                
         }
-    }, [display, pushOperator])
+    }, [display, pushOperator, dispatch])
 
     useEffect(() =>{
-        setDisplay(total);
-    }, [total])
+        dispatch(setDisplay(total));
+    }, [total, dispatch]) */
 
     //console.log(prevNumb, nextNumb, operatorValue, pushOperator)
-    function displayNumber(e){
+    /* function displayNumber(e){
         if(pushOperator){
             setDisplay( e.currentTarget.childNodes[0].innerHTML);            
             setPushOperator(false);
         } else {
             setDisplay(display + e.currentTarget.childNodes[0].innerHTML);            
         }
-    }
+    } */
 
-    function operators(e){
+    /* function operators(e){
         if(!pushOperator){
             calculate(prevNumb, operatorValue, nextNumb);
         }
@@ -43,9 +46,9 @@ function Container(){
         let operator = e.currentTarget.childNodes[0].innerHTML;
         setOperatorValue(operator);
         setPushOperator(true);           
-    }
+    } */
 
-    function calculate(firstValue, operator, secondValue){
+    /* function calculate(firstValue, operator, secondValue){
         switch(operator){
             case "+":
                 setTotal(parseFloat(firstValue) + parseFloat(secondValue));
@@ -64,20 +67,20 @@ function Container(){
                 break;    
 
         }
-    }
-    function reset(){
+    } */
+    /* function reset(){
         setDisplay("0");
         setNextNumb("");
         setPrevNumb("");
         setOperatorValue("");
         setTotal("0");
         setPushOperator(true);           
-    }
-    function equal(e){
-        calculate(prevNumb, operatorValue, nextNumb);
+    } */
+    /* function equal(e){
+        //calculate(prevNumb, operatorValue, nextNumb);
         setDisplay(total)
         setPushOperator(true);           
-    }
+    } */
 
     
     
@@ -85,17 +88,19 @@ function Container(){
         <section className="container">
             <div className="wrapper">
                 <div id="display">
-                    {display}                 
+                    {props.display}                 
                 </div>
-                <CalcDisplay 
-                    displayNumber={displayNumber}
-                    operators={operators}
-                    reset={reset}
-                    equal={equal}
-                />  
+                <CalcDisplay />  
             </div>
         </section>
     )
 }
 
-export default Container;
+function mapStateToProps(state) {
+    return {
+        display: state.display,
+        //pushOperator: state.pushReducer,
+        //total: state.total
+    }
+}
+export default connect(mapStateToProps)( Container);
