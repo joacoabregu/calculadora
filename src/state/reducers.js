526f68510733426270cf4b612ee9cd52c07798a2
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-09 16:42:04 -0300

    modified all reducers

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,79 +1,81 @@
-import { combineReducers } from 'redux';
+
 import {
-  SET_DISPLAY, 
+  SET_DISPLAY,
+  RESET_DISPLAY, 
   SET_TOTAL, 
-  SET_PREV_NUMB, 
-  SET_NEXT_NUMB, 
+  //SET_PREV_NUMB, 
+  //SET_NEXT_NUMB, 
   SET_OPERATOR,
-  SET_PUSH} from './actionTypes';
+  //SET_PUSH,
+  RESET
+  } from './actionTypes';
 
-function displayReducer (state = "", action){
-    switch(action.type){
-      case SET_DISPLAY:
-        return state + action.payload;
 
+let initialState = {
+  display: "0", 
+  total: "0",
+  prevNumb: "", 
+  nextNumb: "",
+  operator: "",
+  push: false
+}
+
+function calculate(firstValue, operator, secondValue){
+  switch(operator){
+      case "+":
+          return (parseFloat(firstValue) + parseFloat(secondValue));
+      case "-":
+          return (parseFloat(firstValue) - parseFloat(secondValue));
+      case "*":
+          return (parseFloat(firstValue) * parseFloat(secondValue));    
+      case "/":
+          return (parseFloat(firstValue) / parseFloat(secondValue));                
       default:
-        return state;
-    }
+          return ("0");    
+
+  }
 }
 
-function totalReducer (state = "", action){
+
+export default function CalculatorApp(state = initialState, action){
+
   switch(action.type){
+    case SET_DISPLAY:
+      let newDisplay = state.display + action.payload;
+      return { ...state, display: newDisplay};
+    case RESET_DISPLAY:
+      if(state.nextNumb){
+        return { ...state, display: action.payload, push: true, prevNumb: state.total};
+      }
+      return { ...state, display: action.payload, push: true};
     case SET_TOTAL:
-      return action.payload;
-
-    default:
-      return state;
-  }
-}
-
-function prevNumbReducer (state = "", action){
-  switch(action.type){
-    case SET_PREV_NUMB:
-      return action.payload;
-
-    default:
-      return state;
-  }
-}
-
-function nextNumbReducer (state = "", action){
-  switch(action.type){
+      let newTotal = calculate(state.prevNumb, state.operator, state.display)
+      return {...state, display: newTotal, total: newTotal, prevNumb: newTotal, nextNumb: "", push: false};
+    /* case SET_PREV_NUMB:
+      let newPrev = state.display
+      return {...state, prevNumb: newPrev};
     case SET_NEXT_NUMB:
-      return action.payload;
-
-    default:
-      return state;
-  }
-}
-
-function operatorReducer (state = "", action){
-  switch(action.type){
+      let newNext = state.display
+      return {...state, prevNumb: newNext}; */
     case SET_OPERATOR:
-      return action.payload;
+      if(state.push && state.total !== "0"){
+         let newTotal = calculate(state.prevNumb, state.operator, state.display) 
+        return {...state, operator: action.payload, prevNumb: newTotal, nextNumb: state.display, total: newTotal, push: false}
+      } 
+       if(state.push && state.prevNumb){
+        let newTotal = calculate(state.prevNumb, state.operator, state.display)
+      return {...state, operator: action.payload, push: false, nextNumb: state.display, total: newTotal};
+      } 
+      return {...state, operator: action.payload, push: false, prevNumb: state.display};
+    /* case SET_PUSH:
+      return {...state, push: action.payload}; */
+    
+    case RESET:
+      return initialState;
+
 
     default:
       return state;
   }
+
 }
-
-function pushReducer (state = true, action){
-  switch(action.type){
-    case SET_PUSH:
-      return action.payload;
-
-    default:
-      return state;
-  }
-}
-
-const CalculatorApp = combineReducers({
-  displayReducer,
-  totalReducer,
-  prevNumbReducer,
-  nextNumbReducer,
-  operatorReducer,
-  pushReducer
-})
-
-export default CalculatorApp;
